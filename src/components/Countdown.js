f7cb3b743c79552560e2047fd065a5f9f7e3ae6b
Elliot Spaull <elliotspaull@gmail.com>
2019-07-04 19:45:24 +0100

    Initial commit!

---
+++ src/components/Countdown.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import dayjs from 'dayjs';
+
+function Countdown() {
+    const calculateTime = () => {
+
+    };
+    
+    return (
+        <div>Gonna do a countdown yo!</div>
+    )
+}
+
+export default Countdown;
